29ddaac87de77b5ac5298ae0a7782f845e621fe6
Brian Woo <woo.brian@gmail.com>
2017-01-26 22:02:12 +0000

    First Commit

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    
+    <div class="test">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {
+                title: "Hello World",
+                user: {
+                    first_name: "John",
+                    last_name: "Doe"
+                }
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
